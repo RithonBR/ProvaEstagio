b62a22abf42d66d6710734feee794e7d7d2c9821
Luciano Dos Santos Cardoso Junior <48662243+RithonBR@users.noreply.github.com>
2021-12-13 12:54:08 -0300

    Delete testes.py

--- testes.py
+++
@@ -1,6 +0,0 @@
-idprod=1
-    print("Product Id :",idprod)
-    nomenovoprod = str(input("Product's name:"))
-    nomenovoprod.title()
-    precoDoProduto = float(input("Product's price :"))
-    qntDoProduto = int(input("Amount :"))
